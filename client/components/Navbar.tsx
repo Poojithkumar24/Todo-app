abc1061cae92d25b6cc7605a0c07ad3fd7434f8b
poojith kumar <poojithk956@gmail.com>
2024-03-27 00:30:26 +0530

    commit changes

---
+++ client/components/Navbar.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { useRouter } from 'next/navigation';
+import { removeToken } from '@/utils/auth';
+
+const Navbar = () => {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    removeToken();
+    router.push('/login');
+  };
+
+  return (
+    <div className="flex justify-between items-center mb-8 p-4 bg-gradient-to-r from-blue-500 to-green-500 rounded-md shadow-md">
+      <h1 className="text-3xl font-semibold text-white">Tasks</h1>
+      <div className="space-x-4">
+        <button
+          onClick={handleLogout}
+          className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md shadow-md transition duration-300 ease-in-out"
+        >
+          Logout
+        </button>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
